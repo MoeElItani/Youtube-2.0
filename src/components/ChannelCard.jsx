701d0f1ebe719fadd02ad2ea1a31123fa9a9689f
MoeElItani <mhmd98itani@gmail.com>
2022-10-13 19:49:14 +0300

    channel card fixed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+// Imports
+import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../utils/constants'
+// Material UI
+import {
+   Box,
+   CardContent,
+   CardMedia,
+   Typography,
+} from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
+const ChannelCard = ({ channelDetail }) => {
+   return (
+      <Box
+         sx={{
+            boxShadow: 'none',
+            width: { md: '300px', xs: '100%' },
+            height: '326px',
+            borderRadius: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+         }}
+      >
+         <Link
+            to={`/channel/${channelDetail?.id?.channelId}`}
+         >
+            <CardContent
+               sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                  color: '#fff',
+               }}
+            >
+               <CardMedia
+                  image={
+                     channelDetail?.snippet?.thumbnails
+                        ?.high?.url || demoProfilePicture
+                  }
+                  alt={channelDetail?.snippet.title}
+                  sx={{
+                     borderRadius: '50%',
+                     height: '180px',
+                     width: '180px',
+                     mb: 2,
+                     border: '1px solid #e3e3e3',
+                  }}
+               />
+               <Typography variant='h6'>
+                  {channelDetail?.snippet?.title}
+                  <CheckCircle
+                     sx={{
+                        fontSize: 14,
+                        color: 'gray',
+                        ml: '5px',
+                     }}
+                  />
+               </Typography>
+               {channelDetail?.statistics
+                  ?.subscriberCount && (
+                  <Typography>
+                     {parseInt(
+                        channelDetail?.statistics
+                           ?.subscriberCount
+                     ).toLocaleString()}{' '}
+                     Subscribers
+                  </Typography>
+               )}
+            </CardContent>
+         </Link>
+      </Box>
+   )
 }
 
 export default ChannelCard
